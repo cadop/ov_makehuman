f03b79c96e41e962d40d16242bc519db287daee3
Grebler <jhg29@njit.edu>
2024-06-03 15:55:45 -0400

    separate skeleton module for clarity

--- exts/siborg.create.human/scripts/buildusd/buildusd.py
+++ exts/siborg.create.human/scripts/buildusd/buildusd.py
@@ -8,15 +8,16 @@ from dataclasses import dataclass
 from typing import List
 import json
 from collections import defaultdict
+from skeleton import build_skeleton
+
 
 def make_human():
-
     # Create a stage
     stage = Usd.Stage.CreateInMemory()
 
     # Stage must have a valid start time code for animation to work
     stage.SetStartTimeCode(1)
-    
+
     # Set the units/scale
     UsdGeom.SetStageMetersPerUnit(stage, 1.0)
 
@@ -29,7 +30,6 @@ def make_human():
     skel_root = UsdSkel.Root.Define(stage, rootPath)
     # Add custom data to the prim by key, designating the prim is a human
     skel_root.GetPrim().SetCustomDataByKey("human", True)
-    
 
     # Load the base mesh from a file
     ext_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,22 +39,8 @@ def make_human():
     for m in meshes:
         create_geom(stage, rootPath.AppendChild(m.name), m)
 
-    # Get all meshes that are not joints
-    non_joint_meshes = [m for m in skel_root.GetPrim().GetChildren() if m.IsA(UsdGeom.Mesh) and not m.GetName().startswith("joint")]
-    rig = load_skel_json(os.path.join(ext_path, "data","rigs","cmu_mb.mhskel"))
-    verts = np.array(non_joint_meshes[0].GetPrim().GetAttribute("points").Get())
-    skeleton = create_skeleton(stage, skel_root, rig, verts)
-
-    weights_json = os.path.join(ext_path, "data","rigs","weights.cmu_mb.json")
-    joint_indices, weights = vertices_to_weights(skeleton.GetJointNamesAttr().Get(), weights_json, skel_root.GetPrim().GetChildren()[0])
-    elements = joint_indices.shape[1]
-
-    # bind the skeleton to each non-joint mesh
-    for mesh in non_joint_meshes:
-        meshBinding = UsdSkel.BindingAPI.Apply(mesh.GetPrim())
-        meshBinding.CreateSkeletonRel().AddTarget(skeleton.GetPrim().GetPath())
-        meshBinding.CreateJointIndicesPrimvar(constant=False, elementSize=elements).Set(joint_indices)
-        meshBinding.CreateJointWeightsPrimvar(constant=False, elementSize=elements).Set(weights)
+    # Create the skeleton
+    skeleton = build_skeleton(stage, skel_root, ext_path)
 
     prim = skel_root.GetPrim()
     # Import the modifiers
@@ -94,7 +80,7 @@ def make_human():
 
     # Create a resizing skeleton for scaling. When blendshapes are applied, we will update the resizing skeleton
     # joints in the rest pose, and then transfer the bone lengths to the original skeleton.
-    resize_skel = create_skeleton(stage, skel_root, rig, verts, "resize_skeleton")
+    resize_skel = build_skeleton(stage, skel_root, ext_path, "resize_skeleton")
     # Move the original skeleton to the resizing skeleton's rest pose
     resizeSkelQuery = skelcache.GetSkelQuery(resize_skel)
     resize_skel_restxforms = resizeSkelQuery.ComputeJointLocalTransforms(0)
@@ -111,6 +97,7 @@ def make_human():
     save_path = os.path.join(ext_path, "data", "human_base.usd")
     print(f"Saving to {save_path}")
     stage.Export(save_path)
+
 
 def combine_joint_meshes(meshes):
     joints, non_joints = [], []
@@ -139,83 +126,8 @@ def combine_joint_meshes(meshes):
     return meshes
 
 
-def create_skeleton(stage, skel_root, rig, mesh_verts, name = "skeleton") -> UsdSkel.Skeleton:
-    # Define a Skeleton, and associate with root.
-    rootPath = skel_root.GetPath()
-    skeleton = UsdSkel.Skeleton.Define(stage, rootPath.AppendChild(name))
-    rootBinding = UsdSkel.BindingAPI.Apply(skel_root.GetPrim())
-    rootBinding.CreateSkeletonRel().AddTarget(skeleton.GetPrim().GetPath())
-
-    # Determine the root, which has no parent. If there are multiple roots, use the last one.
-    root = [name for name, item in rig.items() if item["parent"] == None][-1]
-
-    visited = []  # List to keep track of visited bones.
-    queue = [[root, rig[root]]]  # Initialize a queue
-    path_queue = [root]  # Keep track of paths in a parallel queue
-    joint_paths = [root]  # Keep track of joint paths
-    joint_names = [root]  # Keep track of joint names
-    helper_vertices = {}  # Keep track of helper geometry (vertices)
-
-    # Compute the root transforms
-    root_vert_idxs = rig[root]["head_vertices"]
-    root_vertices = mesh_verts[root_vert_idxs]
-    root_rest_xform, root_bind_xform = compute_transforms(mesh_verts, root_vertices)
-
-    bind_xforms = [Gf.Matrix4d(root_bind_xform)]  # Bind xforms are in world space
-    rest_xforms = [Gf.Matrix4d(root_rest_xform)]  # Rest xforms are in local space
-
-    helper_vertices[root] = root_vert_idxs
-    # Traverse skeleton (breadth-first) and store joint data
-    while queue:
-        v = queue.pop(0)
-        path = path_queue.pop(0)
-        for neighbor in v[1]["children"].items():
-            if neighbor[0] not in visited:
-                visited.append(neighbor[0])
-                queue.append(neighbor)
-                child_path = path+"/"+Tf.MakeValidIdentifier(neighbor[0])
-                path_queue.append(child_path)
-                joint_paths.append(child_path)
-                joint_names.append(neighbor[0])
-                vert_idxs = neighbor[1]["head_vertices"]
-                helper_vertices[child_path] = vert_idxs
-                parent_vert_idxs = v[1]["head_vertices"]
-                vertices = mesh_verts[np.array(vert_idxs)]
-                parent_vertices = mesh_verts[np.array(parent_vert_idxs)]
-                rest_xform, bind_xform = compute_transforms(vertices, parent_vertices)
-                rest_xforms.append(Gf.Matrix4d(rest_xform))
-                bind_xforms.append(Gf.Matrix4d(bind_xform))
-
-
-    skeleton.CreateJointNamesAttr(joint_names)
-    skeleton.CreateJointsAttr(joint_paths)
-    skeleton.CreateBindTransformsAttr(bind_xforms)
-    skeleton.CreateRestTransformsAttr(rest_xforms)
-    skeleton.GetPrim().SetCustomData(helper_vertices)
-    return skeleton
-
-
-
-def compute_transforms(head_vertices, parent_vertices=None):
-    """Compute the rest and bind transforms for a joint"""
-    head_position = np.mean(head_vertices, axis=0)
-    # Bind transform is in world space
-    bind_transform = np.eye(4)
-    bind_transform[:3, 3] = head_position
-
-    # If a parent head is provided, adjust the head to be in local space.
-    if parent_vertices is not None:
-        local_head = head_position - np.mean(parent_vertices, axis=0)
-    else:
-        local_head = head_position
-    rest_transform = np.eye(4)
-    rest_transform[:3, 3] = local_head
-
-    return rest_transform.T, bind_transform.T
-
-
-class TargetModifier():
-    """ A class holding the data and methods for a modifier that targets specific blendshapes.
+class TargetModifier:
+    """A class holding the data and methods for a modifier that targets specific blendshapes.
     blend: str
         The base name of the blendshape(s) to modify
     min_blend: str, optional
@@ -225,12 +137,13 @@ class TargetModifier():
     min_val: float, optional
         The minimum value for the parameter. By default 0
     max_val: float, optional
-        The maximum value for the parameter. By default 1    
+        The maximum value for the parameter. By default 1
     image: str, optional
         The path to the image to use for labeling. By default None
     label: str, optional
         The label to use for the modifier. By default is target basename capitalized.
     """
+
     def __init__(self, group, modifier_data: dict):
         if "target" in modifier_data:
             tlabel = modifier_data["target"].split("-")
@@ -249,7 +162,7 @@ class TargetModifier():
             print(f"No target for modifier {self.full_name}. Is this a macrovar modifier?")
             return
         # Blendshapes are named based on the modifier name
-        self.blend = Tf.MakeValidIdentifier(modifier_data["target"])        
+        self.blend = Tf.MakeValidIdentifier(modifier_data["target"])
         self.min_blend = None
         self.max_blend = None
         if "min" in modifier_data and "max" in modifier_data:
@@ -263,6 +176,7 @@ class TargetModifier():
             self.min_val = 0
         # Modifiers either in the range [0,1] or [-1,1]
         self.max_val = 1
+
 
 def import_modifiers(prim, modifiers_path):
     """Import modifiers from a JSON file. Write customdata to the prim to store the modifiers."""
@@ -294,7 +208,7 @@ def import_modifiers(prim, modifiers_path):
                 "max_val": modifier.max_val,
                 "blend": modifier.blend,
                 "min_blend": modifier.min_blend,
-                "max_blend": modifier.max_blend
+                "max_blend": modifier.max_blend,
             }
             # Remove None values
             modifier_custom_data = {k: v for k, v in modifier_custom_data.items() if v is not None}
@@ -302,7 +216,7 @@ def import_modifiers(prim, modifiers_path):
             prim.SetCustomDataByKey("modifiers", groups_custom_data)
 
 
-def mhtarget_to_blendshapes(stage, prim, path : str) -> [Sdf.Path]:
+def mhtarget_to_blendshapes(stage, prim, path: str) -> [Sdf.Path]:
     """Import a blendshape from a MakeHuman target file.
 
     Parameters
@@ -334,7 +248,7 @@ def mhtarget_to_blendshapes(stage, prim, path : str) -> [Sdf.Path]:
 
     # Create a prim to hold the blendshapes. It's just a container for the blendshapes so it doesn't need a type.
     targets_prim = stage.DefinePrim(prim.GetPath().AppendChild("targets"))
-    path_components = path.split(os.path.sep)[path.split(os.path.sep).index("targets")+1:-1]
+    path_components = path.split(os.path.sep)[path.split(os.path.sep).index("targets") + 1 : -1]
     group_name = Tf.MakeValidIdentifier(path_components[0])
     target_basename = os.path.splitext(os.path.basename(path))[0]
     prefix = "_".join(path_components[1:]) or ""
@@ -361,7 +275,7 @@ def mhtarget_to_blendshapes(stage, prim, path : str) -> [Sdf.Path]:
     blendshape.CreatePointIndicesAttr().Set(indices)
 
     # Get all the meshes. We need to determine which meshes are affected by this target
-    
+
     meshes = [child for child in prim.GetChildren() if child.IsA(UsdGeom.Mesh)]
 
     for mesh in meshes:
@@ -394,6 +308,7 @@ def mhtarget_to_blendshapes(stage, prim, path : str) -> [Sdf.Path]:
                 print(f"Blendshapes {mismatched} are bound but do not exist")
             # Set the updated blendshapes for this mesh.
             meshBinding.GetBlendShapesAttr().Set(existing_blendshapes)
+
 
 @dataclass
 class MeshData:
@@ -433,7 +348,7 @@ def create_geom(stage, path: str, mesh_data: MeshData):
     # same as the number of faces
     #   Example: 4 faces with 4 vertices each
     #   meshGeom.CreateFaceVertexCountsAttr([4, 4, 4, 4])
-    # 
+    #
     #   Example: 4 faces with varying number of vertices
     #   meshGeom.CreateFaceVertexCountsAttr([3, 4, 5, 6])
 
@@ -469,13 +384,13 @@ def create_geom(stage, path: str, mesh_data: MeshData):
 
     return meshGeom.GetPrim()
 
-def load_obj(filename, nPerFace=None):
 
-    with open(filename, 'r') as infile:
+def load_obj(filename, nPerFace=None):
+    with open(filename, "r") as infile:
         lines = infile.readlines()
 
     # Remove comments
-    newdata = [x.rstrip('\n').split() for x in lines if '#' not in x]
+    newdata = [x.rstrip("\n").split() for x in lines if "#" not in x]
 
     vertices = []
     uvs = []
@@ -488,29 +403,31 @@ def load_obj(filename, nPerFace=None):
     nface_verts = []
 
     for i, ln in enumerate(newdata):
-        if ln[0] == 'v':
+        if ln[0] == "v":
             vertices.append(tuple(ln[1:]))
-        elif ln[0] == 'vt':
+        elif ln[0] == "vt":
             uvs.append(ln[1:])
-        elif ln[0] == 'f':
+        elif ln[0] == "f":
             if nPerFace:
                 if nPerFace > len(ln[1:]):
-                    raise ValueError(f'Face has less than {nPerFace} vertices')
-                faces.append(ln[1:nPerFace+1])  # Only consider the first nPerFace vertices
+                    raise ValueError(f"Face has less than {nPerFace} vertices")
+                faces.append(ln[1 : nPerFace + 1])  # Only consider the first nPerFace vertices
                 nface_verts.append(nPerFace)
             else:
-                faces.append(ln[1:]) # Consider all vertices
+                faces.append(ln[1:])  # Consider all vertices
                 nface_verts.append(len(ln[1:]))
-        elif ln[0] == 'g':
+        elif ln[0] == "g":
             # Record the accumulated data and start a new group
-                # Flat lists of face vertex indices
+            # Flat lists of face vertex indices
             if group:
                 for face in faces:
                     for i in range(len(face)):
-                        vert_indices.append(int(face[i].split('/')[0]) - 1)
-                        uv_indices.append(int(face[i].split('/')[1]) - 1)
+                        vert_indices.append(int(face[i].split("/")[0]) - 1)
+                        uv_indices.append(int(face[i].split("/")[1]) - 1)
 
-                mesh_data.append(MeshData(group, None, None, None, vert_indices, uv_indices, normal_indices, nface_verts))
+                mesh_data.append(
+                    MeshData(group, None, None, None, vert_indices, uv_indices, normal_indices, nface_verts)
+                )
             faces = []
             vert_indices = []
             uv_indices = []
@@ -530,63 +447,49 @@ def load_obj(filename, nPerFace=None):
 
     return mesh_data
 
-def load_skel_json(rig_json: str) -> dict:
-    """Load a skeleton from JSON files"""
 
-    dirname = os.path.dirname(rig_json)
+# def load_obj(filename)
+#     # Read the file
+#     with open(filename, 'r') as f: data = f.readlines()
 
-    with open(rig_json, 'r') as f:
-        skel_data = json.load(f)
-    weights_json = os.path.join(dirname, skel_data["weights_file"])
-    with open(weights_json, 'r') as f:
-        weights_data = json.load(f)
-        weights_data = weights_data["weights"]
-    # Root bone has no parent
-    return build_tree(None, skel_data, weights_data)
+#     # Remove comments
+#     newdata = [x.rstrip('\n').split() for x in data if '#' not in x]
+#     verts = np.asarray([x[1:] for x in newdata if x[0]=='v'], float)
+#     idx = np.arange(len(verts))
+#     uv = np.asarray([x[1:] for x in newdata if x[0]=='vt'], float)
+#     face = np.asarray([x[1:] for x in newdata if x[0]=='f']) # This should fail if it creates a ragged array
+#     face = np.apply_along_axis(lambda x: [y.split('/') for y in x], 0, face)
+#     # Get the face number without vertex coordinate
+#     face = np.asarray(face[:,0,:], int)
 
-def build_tree(node_name, skel_data, weight_data):
-    """Recursively build the tree structure and integrate vertex weights."""
-    children = {}
-    for name, item in skel_data["bones"].items():
-        if item["parent"] == node_name:
-            children[name] = item
-            child = children[name]
-            child["head_vertices"] = skel_data["joints"][child["head"]]
-            child["tail_vertices"] = skel_data["joints"][child["tail"]]
+#     obj_types = [x[0] for x in newdata]
+#     nptype = np.asarray(obj_types)
 
-    subtree = {}
-    for child_name in children:
-        subtree[child_name] = children[child_name]
-        subtree[child_name]["children"] = build_tree(child_name, skel_data, weight_data)
-        subtree[child_name]["vertex_weights"] = weight_data.get(child_name, [])  # Get vertex weights if available, else an empty list
-    return subtree
+#     print(nptype)
 
+#     idx = np.where(nptype == 'g', 1, 0)
+#     idx = np.asarray(idx, dtype=int)
+#     idx = np.nonzero(idx)
 
-def vertices_to_weights(joint_names: List[str], weights_json: str, mesh: UsdGeom.Mesh):
-    """Returns, in vertex order, a list of joints and their weights for each vertex"""
-    vertices = mesh.GetAttribute("points").Get()
-    joint_names = list(joint_names)
-    with open(weights_json, 'r') as f:
-        weights_data = json.load(f)
-        weights_data = weights_data["weights"]
-    joint_indices = [[] for _ in range(len(vertices))]
-    joint_weights = [[] for _ in range(len(vertices))]
-    for joint in joint_names:
-        if joint not in weights_data:
-            continue
-        for vertex_data in weights_data[joint]:
-            idx = vertex_data[0]
-            weight = vertex_data[1]
-            joint_indices[idx].append(joint_names.index(joint))
-            joint_weights[idx].append(weight)
-    # Make the array rectangular
-    max_len = max(len(x) for x in joint_indices)
-    joint_indices = np.array([x + [0]*(max_len-len(x)) for x in joint_indices])
-    joint_weights = np.array([x + [0]*(max_len-len(x)) for x in joint_weights])
-    # Normalize the weights
-    joint_weights = joint_weights / np.sum(joint_weights, axis=1)[:, None]
-    return joint_indices, joint_weights
+#     print(idx)
 
+#     1/0
+
+#     group_data = []
+#     active_group = False
+
+#     # Go through the file and find the group ranges
+#     for i, ln in enumerate(newdata):
+#         if ln[0] =='g':
+#             # record the body name and index
+#             if not active_group:
+#                 group_data.append([ln[1], i])
+#                 active_group = True
+#             # Set the end index
+#             elif active_group:
+#                 group_data[-1].extend([i])
+#                 active_group = False
+#     print(group_data)
 
 if __name__ == "__main__":
     make_human()
