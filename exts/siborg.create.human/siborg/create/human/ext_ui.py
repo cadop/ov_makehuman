3469ed60fc35c9a749c90366323e0bb7c3e1dc62
Grebler <jhg29@njit.edu>
2024-06-13 14:36:44 -0400

    reduced widget complexity. Recombine.

--- exts/siborg.create.human/siborg/create/human/ext_ui.py
+++ exts/siborg.create.human/siborg/create/human/ext_ui.py
@@ -3,7 +3,6 @@ from typing import List, Dict
 from pxr import Usd, Tf
 from siborg.create.human.shared import data_path
 from . import mhusd
-from .widgets import SliderEntry
 from . import styles
 
 
@@ -163,3 +162,57 @@ class NoSelectionNotification:
         messages = message.split("\n")
         self._message_label.text = messages[0]
         self._suggestion_label.text = messages[1]
+
+
+class SliderEntry:
+    def __init__(
+        self,
+        label: str,
+        model: ui.SimpleFloatModel,
+        min: float = 0,
+        max: float = 1,
+        default: float = 0,
+    ):
+        """Constructs an instance of SliderEntry
+
+        Parameters
+        ----------
+        label : str
+            Label to display for slider/field
+        model : ui.SimpleFloatModel
+            Model to publish changes to
+        min : float, optional
+            Minimum value, by default None
+        max : float, optional
+            Maximum value, by default None
+        default : float, optional
+            Default parameter value, by default 0
+        """
+        self.label = label
+        self.model = model
+        self.min = min
+        self.max = max
+        self.default = default
+        self._build_widget()
+
+    def _build_widget(self):
+        """Construct the UI elements"""
+        with ui.HStack(height=0, style=styles.sliderentry_style):
+            # Stack the label and slider on top of each other
+            with ui.VStack(spacing=5):
+                ui.Label(
+                    self.label,
+                    height=15,
+                    alignment=ui.Alignment.CENTER,
+                    name="label_param",
+                )
+                # Limit drag values to within min and max if provided
+                if self.min and self.max:
+                    self.drag = ui.FloatSlider(
+                        model=self.model,
+                        step=0.01,
+                        min_value=self.min,
+                        max_value=self.max,
+                    )
+                    return
+                self.drag = ui.FloatSlider(model=self.model, step=0.01)
